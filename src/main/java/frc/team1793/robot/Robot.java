3f0cfa2ff3d33ac0c99aa604fe45308d0405de6d
team1793 <team1793@gmail.com>
2020-01-25 14:36:59 -0500

    AHHHHHHHHHHHHH initalize

---
+++ src/main/java/frc/team1793/robot/Robot.java
@@ -0,0 +1,32 @@
+package frc.team1793.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+
+public class Robot extends TimedRobot {
+
+    @Override
+    public void robotPeriodic() {
+    }
+
+    @Override
+    public void autonomousInit() {
+
+    }
+
+
+    @Override
+    public void autonomousPeriodic() {
+
+    }
+
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+
+    @Override
+    public void testPeriodic() {
+    }
+}
